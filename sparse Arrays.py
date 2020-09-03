4c1a3e87dedf4fa9602f0290b948e81698d5b36c
rnayan78 <56041285+rnayan78@users.noreply.github.com>
2020-09-03 16:20:07 +0530

    Create sparse Arrays.py

---
+++ sparse Arrays.py
@@ -0,0 +1,74 @@
+"""
+There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings.
+
+For example, given input  and , we find  instances of ',  of '' and  of ''. For each query, we add an element to our return array, .
+
+Function Description
+
+Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+
+matchingStrings has the following parameters:
+
+strings - an array of strings to search
+queries - an array of query strings
+Input Format
+
+The first line contains and integer , the size of .
+Each of the next  lines contains a string .
+The next line contains , the size of .
+Each of the next  lines contains a string .
+
+Constraints
+
+
+
+ .
+
+Output Format
+
+Return an integer array of the results of all queries in order."""
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+new = []
+def matchingStrings(strings, queries):
+    new=[]
+    for i in range(len(queries)):
+
+        counter = 0
+        for j in range(len(strings)):
+            if queries[i]==strings[j]:
+
+                counter +=1
+        new.append(counter)
+    return new
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
